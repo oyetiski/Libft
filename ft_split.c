c1e9be50ee9daee077b65909668197768be10a4d
Olcay Yetiskin <olyetisk@k1m02s04.42istanbul.com.tr>
2024-05-24 16:13:05 +0300

    bit

---
+++ ft_split.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olyetisk <olyetisk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 15:10:58 by olyetisk          #+#    #+#             */
+/*   Updated: 2023/12/13 16:00:03 by olyetisk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_count_wrd(char const *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+char	*ft_do_wrd(char *word, char const *s, int j, int word_ln)
+{
+	int	i;
+
+	i = 0;
+	while (word_ln > 0)
+		word[i++] = s[j - word_ln--];
+	word[i] = 0;
+	return (word);
+}
+
+char	**ft_words(char **res, char const *s, char c, int word_ct)
+{
+	int	i;
+	int	j;
+	int	word_ln;
+
+	i = 0;
+	j = 0;
+	word_ln = 0;
+	while (s[j] && i < word_ct)
+	{
+		while (s[j] && s[j] == c)
+			j++;
+		while (s[j] && s[j] != c)
+		{
+			j++;
+			word_ln++;
+		}
+		res[i] = (char *)malloc(sizeof(char) * (word_ln + 1));
+		if (!res[i])
+			return (0);
+		ft_do_wrd(res[i], s, j, word_ln);
+		word_ln = 0;
+		i++;
+	}
+	res[i] = 0;
+	return (res);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		word_ct;
+	char	**res;
+
+	if (s == 0)
+		return (0);
+	word_ct = ft_count_wrd(s, c);
+	res = (char **)malloc(sizeof(char *) * (word_ct + 1));
+	if (!res)
+		return (0);
+	ft_words(res, s, c, word_ct);
+	return (res);
+}
